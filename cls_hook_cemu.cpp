fd4ecf323c95ccb4bd9a00f6e344a1f169ec68e8
Keith Bourdon <33245078+celerizer@users.noreply.github.com>
2024-08-07 18:23:46 -0500

    Support cemu on linux

--- cls_hook_cemu.cpp
+++ cls_hook_cemu.cpp
@@ -1,9 +1,10 @@
-#ifdef WIN32
-
+#include <QMessageBox>
 #include <QString>
 #include <QtEndian>
 
 #include "cls_hook_cemu.h"
+
+ClsHookCemu::ClsHookCemu(unsigned pid, const cls_window_preset_t *preset) : ClsHook(pid, preset) {}
 
 static bool get_title_id(cl_identify_cafe_t *ident, const QString &str)
 {
@@ -32,10 +33,10 @@ static bool get_title_id(cl_identify_cafe_t *ident, const QString &str)
   QString version_string = str.mid(v_pos + 1,
                                    position - v_pos - 1);
   bool ok;
-  uint16_t version = version_string.toUShort(&ok, 10);
+  uint64_t version = version_string.toUShort(&ok, 10);
   if (!ok)
     return false;
-  qToBigEndian(version, &ident->version);
+  cl_read(&ident->version, (uint8_t*)&version, 0, sizeof(version), CL_ENDIAN_BIG);
 
   /* Get the title ID by reading between the next brackets */
   int closed_pos = str.lastIndexOf(']', v_pos);
@@ -46,14 +47,15 @@ static bool get_title_id(cl_identify_cafe_t *ident, const QString &str)
     return false;
 
   /* Copy it into the identification struct and set the remainder to 00 */
-  uint64_t title_id = title_id_string.toULongLong(&ok, 16);
+  uint64_t title_id = title_id_string.toULong(&ok, 16);
   if (!ok)
     return false;
-  qToBigEndian(title_id, &ident->title_id);
+  cl_read(&ident->title_id, (uint8_t*)&title_id, 0, sizeof(title_id), CL_ENDIAN_BIG);
 
   return true;
 }
 
+#if WIN32
 uintptr_t GetModuleBaseAddress(unsigned process_id, const wchar_t* module_name)
 {
   HANDLE th = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, process_id);
@@ -79,32 +81,30 @@ uintptr_t GetModuleBaseAddress(unsigned process_id, const wchar_t* module_name)
 
   return 0;
 }
-
-ClsHookCemu::ClsHookCemu(const cls_window_preset_t *preset)
-{
-  m_Preset = preset;
-}
+#endif
 
 bool ClsHookCemu::getIdentification(uint8_t **data, unsigned int *size)
 {
   char window_title[256];
 
-  GetWindowTextA(m_Window, window_title, sizeof(window_title));
-  if (!get_title_id(&m_Identification, QString(window_title)))
+  if (!getWindowTitle(window_title, sizeof(window_title)))
+    return false;
+  else if (!get_title_id(&m_Identification, QString(window_title)))
     return false;
   else
   {
     *data = reinterpret_cast<uint8_t*>(&m_Identification);
     *size = sizeof(m_Identification);
+
     return true;
   }
 }
 
 bool ClsHookCemu::init()
 {
+#if CL_HOST_PLATFORM == CL_PLATFORM_WINDOWS
   if (!ClsHook::init())
     return false;
-
   m_AddressCemuModule = GetModuleBaseAddress(m_ProcessId, L"Cemu.exe");
   if (!m_AddressCemuModule)
     return false;
@@ -115,42 +115,15 @@ bool ClsHookCemu::init()
     return false;
 
   return true;
+#else
+  return initViaMemoryRegions({0x4E000000, 0x0E000000});
+#endif
 }
 
 bool ClsHookCemu::run()
 {
-  if (!m_AddressCemuModule)
-    return false;
-  ClsHook::read(&m_CycleCount, m_AddressCemuModule + 0x13064EC, sizeof(m_CycleCount));
-
-  return m_CycleCount;
+  //if (!m_AddressCemuModule)
+  //  return false;
+  //ClsHook::read(&m_CycleCount, m_AddressCemuModule + 0x13064EC, sizeof(m_CycleCount));
+  return true;
 }
-
-size_t ClsHookCemu::read(void *dest, cl_addr_t address, size_t size)
-{
-  if (!m_AddressForegroundApp)
-    return false;
-  else
-    return ClsHook::read(dest, address + m_AddressForegroundApp, size);
-}
-
-size_t ClsHookCemu::write(const void *src, cl_addr_t address, size_t size)
-{
-  if (!m_AddressForegroundApp)
-    return false;
-  else
-    return ClsHook::write(src, address + m_AddressForegroundApp, size);
-}
-
-bool ClsHookCemu::deepCopy(cl_search_t *search)
-{
-  if (!search || search->searchbank_count != 1 || !search->searchbanks->bank[0].data)
-    return false;
-
-  return read(
-    search->searchbanks[0].bank->data + search->searchbanks[0].first_valid,
-    search->searchbanks[0].first_valid + 0x10000000,
-    search->searchbanks[0].last_valid - search->searchbanks[0].first_valid + search->params.size);
-}
-
-#endif
